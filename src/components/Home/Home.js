acc3cd2c6b9b8eb672a690bcdc00402c9c0c7d8c
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-15 13:44:39 -0700

    updated the Home styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,10 @@ import Button from "./Button";
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Hi, I'm Matt,</h1>
-      <h2>a freelance software engineer based out of Windsor, California.</h2>
+      <h1 className="home-title">Hi, I'm Matt,</h1>
+      <h2 className="home-subtitle">
+        a freelance software engineer based out of Windsor, California.
+      </h2>
       <Button name="PROJECTS" />
       <Button name="RESUME" />
     </div>
