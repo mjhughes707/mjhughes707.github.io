a89005fefc7943f85125b86a9f9d1c2cba8fdbdd
Matthew J. Hughes <mjhughes707@gmail.com>
2020-05-13 18:47:35 -0700

    background height adjustments

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./Home.css";
 import Button from "./Button";
 
-const Home = (props) => {
-  const { navlinks } = props;
-  const height = navlinks
-    ? "calc(100vh - (56px + 151px))"
-    : "calc(100vh - 56px)";
-
+const Home = () => {
   return (
-    <div className="Home" style={{ height: height }}>
+    <div className="Home">
       <h1 className="home-title">Hi, I'm Matt,</h1>
       <h2 className="home-subtitle">
-        a freelance software engineer based out of Windsor, California.
+        a freelance software engineer based out of Windsor, California
       </h2>
       <Button name="PROJECTS" />
       <Button name="RESUME" />
     </div>
   );
-};
-
-Home.propTypes = {
-  navlinks: PropTypes.bool.isRequired,
 };
 
 export default Home;
